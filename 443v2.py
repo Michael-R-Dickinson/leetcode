b8a95a67bd8730e0481ef7bc0b05ad78198579de
MichaelDickinson20I <michael.dickinson3949@gmail.com>
2024-09-25 19:40:46 -0700

    updated 443

--- 443v2.py
+++ 443v2.py
@@ -2,8 +2,21 @@ class Solution:
     def compress(self, chars: List[str]) -> int:
         read_i = 0
         set_i = 0
-        count = 0
 
-        while True:
-            count += 1
-            if (read_i == 0 or chars[read_i] != chars[read_i - 1]):
+        while (read_i < len(chars)):
+            letter = chars[read_i]
+            count = 0
+            while (read_i < len(chars) and chars[read_i] == letter):
+                read_i += 1
+                count += 1
+
+            chars[set_i] = letter
+            set_i += 1
+            print (f'add char {read_i} {letter} {set_i} {count}')
+
+            if (count > 1):
+                for i in str(count):
+                    chars[set_i] = i
+                    set_i += 1
+
+        return set_i
